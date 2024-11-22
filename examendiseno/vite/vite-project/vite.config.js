66033da15bffa5596fabd2dc78f403b07f0d1a17
GuidoFC <guidofigueroa96@gmail.com>
2024-11-22 17:26:19 +0100

    Listo para examen

---
+++ examendiseno/vite/vite-project/vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+export default {
+    // config options
+    base: "",
+    root: "./", // especificamos la carpeta donde esta nuestro index.html
+
+    server: {
+        port: 8888,
+        strictPort: true,
+        // para sincronizar el volumen con lo que tengo en local
+        host: '0.0.0.0', // Exponemos el servidor para que sea accesible en la red
+        watch: {
+            usePolling: true, // Activamos el polling para detectar cambios automáticamente
+        },
+    },
+    build: { // npm run build
+        outDir: "./Dist", // Estamos en la carperta donde pone "root:  " y decimos que cree una carpeta /Dist
+        emptyOutDir: true,
+        minify: false, // para que no se minimice
+    },
+};
